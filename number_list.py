273aeb08a70223abc21b761750b7d5ea6d8acb64
rafaelzanirato <84473402+rafaelzanirato@users.noreply.github.com>
2021-06-09 16:56:51 -0300

    Add files via upload

---
+++ number_list.py
@@ -0,0 +1,14 @@
+
+for value in range(5):
+   print("Rafael")
+print("*****************")
+numbers=list(range(2,10,2))
+print(numbers)
+print("*****************")
+squares=[]
+for value in range(1,11):
+    squares.append("Rafael")
+print(squares)
+print("*****************")
+
+    
